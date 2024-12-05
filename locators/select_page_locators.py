a407bfefaf689f26d5eb7154b3aa32c618e95627
Imabirdmf <vash.stampide@gmail.com>
2024-12-05 17:28:30 +0000

    add new files

---
+++ locators/select_page_locators.py
@@ -0,0 +1,2 @@
+class SelectPageLocators:
+    OLD_STYLE_SELECT_LOC = ('css selector', 'select[id="oldSelectMenu"]')
---
+++ tests/test_select.py
@@ -0,0 +1,13 @@
+from data.urls import Urls
+from pages.select_page import SelectPage
+import time
+
+
+class TestSelectPage:
+    url = Urls()
+
+    def test_select(self, driver):
+        page = SelectPage(driver, self.url.demoqa_select_url)
+        page.open()
+        page.select('Indigo')
+        time.sleep(10)
